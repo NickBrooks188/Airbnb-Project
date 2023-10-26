73db2e9ce6acd82415a5332577db59e8c223568d
Nick Brooks <nicholasbrooks1@msn.com>
2023-10-26 14:25:07 -0700

    Added user authentication express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,12 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
 
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+const { restoreUser } = require('../../utils/auth.js');
 
+router.use(restoreUser);
 
 module.exports = router;
