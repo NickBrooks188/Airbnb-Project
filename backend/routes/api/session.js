1145593c3844547700add0c1d84db33f98aca836
Nick Brooks <nicholasbrooks1@msn.com>
2023-10-26 15:22:10 -0700

    Update session.js

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,9 +6,23 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 
 // Log in
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.unscoped().findOne({
@@ -43,29 +57,25 @@ router.post('/', async (req, res, next) => {
 );
 
 // Log out
-router.delete(
-    '/',
-    (_req, res) => {
-        res.clearCookie('token');
-        return res.json({ message: 'success' });
-    }
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+}
 );
 
-router.get(
-    '/',
-    (req, res) => {
-        const { user } = req;
-        if (user) {
-            const safeUser = {
-                id: user.id,
-                email: user.email,
-                username: user.username,
-            };
-            return res.json({
-                user: safeUser
-            });
-        } else return res.json({ user: null });
-    }
+router.get('/', (req, res) => {
+    const { user } = req;
+    if (user) {
+        const safeUser = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        };
+        return res.json({
+            user: safeUser
+        });
+    } else return res.json({ user: null });
+}
 );
 
 module.exports = router;
