da15b7bf2315df2fda6520497af48954b175b0e7
Nick Brooks <nicholasbrooks1@msn.com>
2023-10-27 11:01:57 -0700

    Added schema setup to first-last-name migration file

--- backend/db/migrations/20231026223530-add-first-last-name.js
+++ backend/db/migrations/20231026223530-add-first-last-name.js
@@ -2,6 +2,11 @@
 
 const { query } = require('express');
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
