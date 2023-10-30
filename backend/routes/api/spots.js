50bd82a66aa7a07889bf277c7a81be5b2d086c62
Nick Brooks <nicholasbrooks1@msn.com>
2023-10-30 14:21:17 -0700

    Mid-bugfix

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,12 +2,62 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot, Review, SpotImage, User } = require('../../db/models');
+const { Spot, Review, SpotImage, User, ReviewImage } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+router.get('/:id/reviews', async (req, res) => {
+    const spot = await Spot.findByPk(req.params.id)
+    // const reviews = await Spot.getReviews()
+    const reviews = await Review.findAll({
+        // include: {
+        //     model: ReviewImage
+        // }
+    })
+    // console.log(reviews)
+    // const spot = await Spot.findByPk(req.params.id, {
+    //     attributes: [],
+    //     include: {
+    //         model: User,
+    //         attributes: ['id'],
+    //         through: {
+    //             model: Review,
+    //             attributes: ['id', 'userId', 'spotId', 'review', 'stars', 'createdAt', 'updatedAt'],
+    //             include: [{
+    //                 model: ReviewImage
+    //             }]
+    //         }
+    //     }
+    // })
+    // if (!spot) {
+    //     res.statusCode = 404
+    //     return res.json({ 'message': 'Spot does not exist' })
+    // }
+    // const reviews = await Review.findAll({
+    //     where: {
+    //         spotId: req.params.id
+    //     },
+    //     attributes: ['id', 'userId', 'spotId', 'review', 'stars', 'createdAt', 'updatedAt'],
+    //     include: [{
+    //         model: User,
+    //         attributes: ['id'],
+    //         where: {
+    //             id: 
+    //         }
+    //     }]
+    // })
+
+    // const reviews = await ReviewImage.findAll()
+
+    return res.json(spot)
+})
+
+router.get('/:id/bookings', async (req, res) => {
+    // const bookings = await
+})
 
 router.get('/:id', async (req, res) => {
     let spot = await Spot.findByPk(req.params.id, {
