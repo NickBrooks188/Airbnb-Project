73ac330c380f23ee117cbea3e8491710596da4e1
Nick Brooks <nicholasbrooks1@msn.com>
2023-11-17 15:45:19 -0800

    Added component folders and route skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,12 @@ function Layout() {
 const router = createBrowserRouter(createRoutesFromElements(
   <Route element={<Layout />}>
     <Route path='/' element={<h1> Spots </h1>} />
+    <Route path='/spots' element={<Outlet />}>
+      <Route path=':spotId' element={<h1>Spot placeholder</h1>} />
+      <Route path='new' element={<h1>Spot form placeholder</h1>} />
+      <Route path='current' element={<h1>User's spots placeholder</h1>} />
+    </Route>
+    <Route path='*' element={<h1>404: Page not found</h1>} />
   </Route>
 ))
 
