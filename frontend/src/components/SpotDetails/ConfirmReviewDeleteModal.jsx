c73cff84b3f3442145b7c75a2f9fc8c6709190ca
Nick Brooks <nicholasbrooks1@msn.com>
2023-11-20 23:01:42 -0800

    Update ConfirmReviewDeleteModal.jsx

--- frontend/src/components/SpotDetails/ConfirmReviewDeleteModal.jsx
+++ frontend/src/components/SpotDetails/ConfirmReviewDeleteModal.jsx
@@ -1,5 +1,27 @@
+import { useDispatch } from 'react-redux'
+import { useModal } from '../../context/Modal'
+
 const ConfirmReviewDeleteModal = () => {
-    return null
+    const { closeModal } = useModal()
+    const dispatch = useDispatch()
+    const confirmDelete = async () => {
+        // const res = await dispatch(removeReview(spotId))
+        // const data = await res.json()
+        // console.log(data)
+        // if (res.ok) {
+        //     await dispatch(removeUserSpot(spotId))
+        //     closeModal()
+        // }
+    }
+
+    return (
+        <>
+            <h1>Confirm Delete</h1>
+            <h3>Are you sure you want to remove this review?</h3>
+            <button className='yesDelete' onClick={confirmDelete}>Yes (Delete Review)</button>
+            <button className='noDelete' onClick={closeModal}>No (Keep Review)</button>
+        </>
+    )
 }
 
 export default ConfirmReviewDeleteModal
